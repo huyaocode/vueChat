a078232e8783adb61c874359906b2fab097c1d7d
wangyunfeifeifei <wangyunfeiafei@outlook.com>
2018-12-30 09:55:40 +0800

    新增获取陌生人列表接口

--- server/controllers/contactList.js
+++ server/controllers/contactList.js
@@ -1,5 +1,9 @@
 const contactModel = require('../models/contactList')
 
+/**
+ * 获取联系人列表
+ * @param {*} ctx
+ */
 const getContactList = async function(ctx) {
   const RowDataPacket = await contactModel.getContactList(ctx.query.user_id),
 	userInfo = JSON.parse(JSON.stringify(RowDataPacket));
@@ -11,6 +15,25 @@ const getContactList = async function(ctx) {
 	};
 }
 
+/**
+ * 获取异性陌生人列表
+ * @param {*} ctx
+ */
+const getStrangerList = async function(ctx) {
+  const RowDataPacket = await contactModel.getStrangerList(ctx.query.sex),
+  userInfo = JSON.parse(JSON.stringify(RowDataPacket));
+  userInfo.sort(() => {
+    return Math.random > 0.5 ? -1 : 1;
+  })
+	ctx.body = {
+		success: true,
+		data: {
+			userInfo: userInfo
+		}
+	};
+}
+
 module.exports = {
-  getContactList
+  getContactList,
+  getStrangerList
 }
